bb37b0547024ef64ae561a0769c6d52edbbae9ee
kgsanjay <91323349+kgsanjay@users.noreply.github.com>
2024-10-29 20:13:09 +0530

    code push

--- digital-navbar.php
+++ digital-navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light shadow-sm">
     <div class="container custom-container">
         <a class="navbar-brand" href="./">
-            <img src="assets/images/logo/sh-group-logo.png" alt="Sankhara Group" height="100"> <!-- Replace with your logo path -->
+            <img src="assets/images/logo/sh-digital-logo1.png" alt="Sankhara Digital" height="100"> <!-- Replace with your logo path -->
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
